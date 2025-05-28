bce94040864f2effaa3689009e7f2fa0eaeea13e
Felipe Werneck <78689294+werneckspx@users.noreply.github.com>
2025-05-28 13:33:09 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,49 +1,120 @@
-CREATE TABLE bronze.crm_cust_info(
-	cst_id INT,
-	cst_key VARCHAR(50),
-	cst_firstname VARCHAR(50),
-	cst_lastname VARCHAR(50),
-	cst_material_status VARCHAR(50),
-	cst_gndr VARCHAR(50),
-	cst_create_date DATE
-);
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+	start_time TIMESTAMP;
+	end_time TIMESTAMP;
+	bronze_start_time TIMESTAMP;
+	bronze_end_time TIMESTAMP;
+BEGIN
+		bronze_start_time := NOW();
+		RAISE NOTICE '======================================';
+		RAISE NOTICE 'Loading Bronze Layer';
+		RAISE NOTICE '======================================';
+	
+		RAISE NOTICE '======================================';
+		RAISE NOTICE 'Loading Crm Tables';
+		RAISE NOTICE '======================================';
+		start_time := NOW();
+		RAISE NOTICE '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+		RAISE NOTICE '>> Inserting Data Into: bronze.crm_cust_info';
+		COPY bronze.crm_cust_info 
+		FROM 'C:/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_crm/cust_info.csv' 
+		WITH (
+			FORMAT csv,
+			HEADER true,
+			DELIMITER ','
+		);
+		end_time := NOW();
+		RAISE NOTICE 'Load duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
 
-CREATE TABLE bronze.crm_prd_info(
-	prd_id INT,
-	prd_key VARCHAR(50),
-	prd_nm VARCHAR(50),
-	prd_cost INT,
-	prd_line VARCHAR(50),
-	prd_start_dt TIMESTAMP,
-	prd_end_dt TIMESTAMP
-);
+		start_time := NOW();
+		RAISE NOTICE '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
+		RAISE NOTICE '>> Inserting Data Into: bronze.crm_prd_info';
+		COPY bronze.crm_prd_info 
+		FROM 'C:/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_crm/prd_info.csv' 
+		WITH (
+			FORMAT csv,
+			HEADER true,
+			DELIMITER ','
+		);
+		end_time := NOW();
+		RAISE NOTICE 'Load duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
 
-CREATE TABLE bronze.crm_sales_details(
-	sls_ord_num VARCHAR(50),
-	sls_prd_key VARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT
-);
 
-CREATE TABLE bronze.erp_loc_a101(
-	cid VARCHAR(50),
-	cntry VARCHAR(50)
-);
+		start_time := NOW();
+		RAISE NOTICE '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+		RAISE NOTICE '>> Inserting Data Into: bronze.crm_sales_details';
+		COPY bronze.crm_sales_details
+		FROM 'C:/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_crm/sales_details.csv' 
+		WITH (
+			FORMAT csv,
+			HEADER true,
+			DELIMITER ','
+		);		
+		end_time := NOW();
+		RAISE NOTICE 'Load duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
 
-CREATE TABLE bronze.erp_cust_az12(
-	cid VARCHAR(50),
-	bdate DATE,
-	gen VARCHAR(50)
-);
+		
+		RAISE NOTICE '======================================';
+		RAISE NOTICE 'Loading Erp Tables';
+		RAISE NOTICE '======================================';
 
-CREATE TABLE bronze.erp_px_cat_g1v2(
-	id VARCHAR(50),
-	cat VARCHAR(50),
-	subcat VARCHAR(50),
-	maintenance VARCHAR(50)	
-);
+		start_time := NOW();
+		RAISE NOTICE '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		RAISE NOTICE '>> Inserting Data Into: bronze.erp_cust_az12';
+		COPY bronze.erp_cust_az12
+		FROM 'C:/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_erp/cust_az12.csv' 
+		WITH (
+			FORMAT csv,
+			HEADER true,
+			DELIMITER ','
+		);		
+		end_time := NOW();
+		RAISE NOTICE 'Load duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
+
+
+		start_time := NOW();
+		RAISE NOTICE '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		RAISE NOTICE '>> Inserting Data Into: bronze.erp_loc_a101';
+		COPY bronze.erp_loc_a101
+		FROM 'C:/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_erp/loc_a101.csv' 
+		WITH (
+			FORMAT csv,
+			HEADER true,
+			DELIMITER ','
+		);		
+		end_time := NOW();
+		RAISE NOTICE 'Load duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
+
+
+		start_time := NOW();
+		RAISE NOTICE '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		RAISE NOTICE '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		COPY bronze.erp_px_cat_g1v2
+		FROM 'C:/sql-data-warehouse-project/sql-data-warehouse-project/datasets/source_erp/px_cat_g1v2.csv' 
+		WITH (
+			FORMAT csv,
+			HEADER true,
+			DELIMITER ','
+		);
+		end_time := NOW();
+		RAISE NOTICE 'Load duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
+
+		bronze_end_time := NOW();
+		RAISE NOTICE 'Load Bronze duration: % segundos', EXTRACT(EPOCH FROM end_time - start_time);
+
+EXCEPTION
+	WHEN OTHERS THEN
+		RAISE NOTICE '=========================';
+		RAISE NOTICE 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		RAISE NOTICE 'ERROR: %', SQLERRM;
+		RAISE NOTICE '=========================';
+END;
+$$;
